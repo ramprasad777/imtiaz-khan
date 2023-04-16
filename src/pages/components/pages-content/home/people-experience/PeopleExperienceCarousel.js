75c79da6444a804748aa7a883e15c18bb07aea08
rpaneja <rpaneja@gmail.com>
2023-04-16 17:45:35 +0530

    home-page|slider responsive fixes

--- src/pages/components/pages-content/home/people-experience/PeopleExperienceCarousel.js
+++ src/pages/components/pages-content/home/people-experience/PeopleExperienceCarousel.js
@@ -75,6 +75,32 @@ const PeopleExperienceCarousel = () =>{
         slidesToScroll: 1,
         centerMode: true,
         centerPadding:'10px',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
     };
     const slidesData = [
         {
